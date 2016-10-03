68b29461cc0869b477f511873088f5526bb597dc
Fabian Thüring <fabian_thuering@hotmail.com>
2016-10-03 10:46:00 +0200

    Refactor python/

--- python/serialbox/__init__.py
+++ python/serialbox/__init__.py
@@ -14,13 +14,14 @@
 ##
 ##===------------------------------------------------------------------------------------------===##
 
-"""'Python Unittest Utilities'"""
+"""'Serialbox Python Interface'"""
 
 from __future__ import absolute_import
 
 __author__ = 'Fabian Thuring'
 __email__ = 'thfabian@student.ethz.ch'
-__versioninfo__ = (0, 1, 0)
-__version__ = '.'.join(str(v) for v in __versioninfo__) + 'dev'
+__versioninfo__ = (2, 0, 1)
+__version__ = '.'.join(str(v) for v in __versioninfo__) + '-dev'
 
 __all__ = []
+
