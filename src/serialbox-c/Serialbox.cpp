4dcbd22975abe6fcee7067f4a49d1a8541583afe
Fabian Thüring <fabian_thuering@hotmail.com>
2016-10-08 18:01:46 +0200

    Refactor CMake and add C Interface infrastructure

---
+++ src/serialbox-c/Serialbox.cpp
@@ -0,0 +1,20 @@
+/*===-- serialbox-c/Serialbox.cpp ---------------------------------------------------*- C++ -*-===*\
+ *
+ *                                    S E R I A L B O X
+ *
+ * This file is distributed under terms of BSD license.
+ * See LICENSE.txt for more information
+ *
+ *===------------------------------------------------------------------------------------------===//
+ *
+ *! \file
+ *! This file contains the unittests for the Binary Archive.
+ *
+\*===------------------------------------------------------------------------------------------===*/
+
+#include "serialbox-c/Serialbox.h"
+#include <iostream>
+
+void serialbox_hello_world() {
+  std::cout << "hello world!" << std::endl;
+}
---
+++ src/serialbox-c/Serialbox.h
@@ -0,0 +1,33 @@
+/*===-- serialbox-c/Serialbox.h -----------------------------------------------------*- C++ -*-===*\
+ *
+ *                                    S E R I A L B O X
+ *
+ * This file is distributed under terms of BSD license.
+ * See LICENSE.txt for more information
+ *
+ *===------------------------------------------------------------------------------------------===//
+ *
+ *! \file
+ *! This file contains the C Interface of Serialbox.
+ *
+\*===------------------------------------------------------------------------------------------===*/
+
+#ifndef SERIALBOX_C_SERIALBOX_H
+#define SERIALBOX_C_SERIALBOX_H
+
+#include "serialbox/Core/Compiler.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief Dummy function
+ */
+void serialbox_hello_world();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
