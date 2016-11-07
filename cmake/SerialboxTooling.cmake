1f9ea342906634cc5a48421fe79c96501fd05ee0
Fabian Thüring <fabian_thuering@hotmail.com>
2016-11-07 14:23:22 +0100

    Add Fortran standalone example

---
+++ cmake/SerialboxTooling.cmake
@@ -0,0 +1,90 @@
+##===------------------------------------------------------------------------------*- CMake -*-===##
+##
+##                                   S E R I A L B O X
+##
+## This file is distributed under terms of BSD license. 
+## See LICENSE.txt for more information.
+##
+##===------------------------------------------------------------------------------------------===##
+
+#.rst:
+# run_pp_ser
+# ----------
+#
+include(CMakeParseArguments)
+
+function(serialbox_run_pp_ser)
+  #===-------------------------------------------------------------------------------------------===
+  # Parse arguments
+  #===-------------------------------------------------------------------------------------------===
+  set(options)
+  set(oneValueArgs OUTPUT_DIR)
+  set(multiValueArgs SOURCES)
+  
+  cmake_parse_arguments(serialbox_add_serialized_executable 
+                        "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  
+  set(output_dir ${serialbox_add_serialized_executable_OUTPUT_DIR})
+  set(sources ${serialbox_add_serialized_executable_SOURCES})
+  
+  set(unparsed ${serialbox_add_serialized_executable_UNPARSED_ARGUMENTS})
+  if(unparsed)
+    message(WARNING "serialbox_add_serialized_executable: unparsed arguments: ${unparsed}")
+  endif()
+  
+  if(NOT(sources))
+    message(FATAL_ERROR "serialbox_add_serialized_executable: no source files provided")
+  endif()
+  
+  if(NOT(output_dir))
+    # If output_dir is not set, we place them in ${CMAKE_BINARY_DIR}/pp
+    set(output_dir ${CMAKE_BINARY_DIR}/pp)  
+  endif()
+  
+  # Create directory if it does not exist
+  file(MAKE_DIRECTORY ${output_dir})
+  
+  #===-------------------------------------------------------------------------------------------===
+  # Run pp_ser
+  #===-------------------------------------------------------------------------------------------===
+  set(pp_ser_input)
+  set(pp_ser_output)
+  foreach(file ${sources})
+    get_filename_component(file_path ${file} ABSOLUTE)
+    get_filename_component(file_name ${file} NAME)
+
+    set(input_file ${file_path})
+    set(output_file ${output_dir}/${file_name})
+    
+    # Output file is out-dated, preprocess again
+    if(NOT(EXISTS(output_file) AND output_file IS_NEWER_THAN input_file))
+      list(APPEND pp_ser_input ${input_file})
+      list(APPEND pp_ser_output ${output_file})
+    endif()
+  endforeach()
+  
+  # Check for pp_ser
+  if(NOT(DEFINED SERIALBOX_PPSER))
+    message(FATAL_ERROR "Set \"SERIALBOX_PPSER\" to the location of the pp_ser.py script")
+  endif()
+
+  # Check for Python
+  if(NOT(DEFINED PYTHONINTERP_FOUND))
+    find_package(PythonInterp REQUIRED)
+  endif()
+
+  if(NOT(PYTHONINTERP_FOUND))
+    message(FATAL_ERROR "pp_ser.py requires a python interpreter")
+  endif()
+  
+  if(pp_ser_input)
+    add_custom_command(OUTPUT ${pp_ser_output}
+                       COMMAND ${PYTHON_EXECUTABLE} ${SERIALBOX_PPSER} --verbose 
+                                                                       --ignore-identical 
+                                                                       --output-dir=${output_dir} 
+                                                                       ${pp_ser_input}
+                       DEPENDS ${pp_ser_input}
+                       COMMENT "Preprocessing for serialization")
+  endif()
+endfunction(serialbox_run_pp_ser)
+
--- examples/fortran/perturbation/README.md
+++ examples/fortran/perturbation/README.md
@@ -1,18 +1,15 @@
 # Serialbox Fortran example
 
-This small example shows how to use Serialbox in a Fortran code using the
-serialization directives.
+This small example shows how to use Serialbox in a Fortran code using the serialization directives.
 
-The serialization happens in the `m_ser.f90` module. Two small programs use
-this module to serialize and deserialize data.
+The serialization happens in the `m_ser.f90` module. Three small programs use this module to serialize and deserialize data.
 
-
-It is important to use the same Fortran compiler than the one used to build 
-Serialbox.
+It is important to use the same tool-chain (C++ and Fortran compiler) than the one used to build Serialbox.
 
 ### Build the example
 ```bash
-cmake .
+mkdir build && cd build
+cmake ../
 make
 ```
 
@@ -21,4 +18,39 @@ make
 ./main_producer
 ./main_consumer
 ./main_consumer_perturb
+```
+
+or
+
+```bash
+bash run.sh
+```
+
+You should see something like:
+
+```bash
+
+Produce serialized data
+
+ CALL serialize with sum(a)=   625.00000000000000
+ >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<
+ >>> WARNING: SERIALIZATION IS ON <<<
+ >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<
+
+Consume serialized data without pertubation
+
+ Before read from serializer: sum(a)=   0.0000000000000000
+ >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<
+ >>> WARNING: SERIALIZATION IS ON <<<
+ >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<
+ After read from serializer: sum(a)=   625.00000000000000
+
+Consume serialized data WITH pertubation
+
+ Before read from serializer: sum(a)=   0.0000000000000000
+ >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<
+ >>> WARNING: SERIALIZATION IS ON <<<
+ >>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<
+ After read from serializer: sum(a)=   624.99993729204868
+
 ```
