fc405f99e82a8d2182a12fde9c14c757112aeb26
Hannes Vogt <hannes@havogt.de>
2017-07-07 10:31:39 +0200

    implement basic StorageViewHelper. need cleanup

--- src/serialbox/core/frontend/gridtools/StorageViewHelper.h
+++ src/serialbox/core/frontend/gridtools/StorageViewHelper.h
@@ -24,6 +24,32 @@ namespace serialbox {
 namespace gridtools {
 
 namespace internal {
+// HACK:
+template <int I>
+struct static_to_vector {
+  template <typename StorageInfo>
+  static void dim(StorageInfo& info, std::vector<int>& v) {
+    v[I] = info.template unaligned_dim<I>();
+    static_to_vector<I - 1>::dim(info, v);
+  }
+  template <typename StorageInfo>
+  static void stride(StorageInfo& info, std::vector<int>& v) {
+    v[I] = info.template stride<I>();
+    static_to_vector<I - 1>::stride(info, v);
+  }
+};
+
+template <>
+struct static_to_vector<0> {
+  template <typename StorageInfo>
+  static void dim(StorageInfo& info, std::vector<int>& v) {
+    v[0] = info.template unaligned_dim<0>();
+  }
+  template <typename StorageInfo>
+  static void stride(StorageInfo& info, std::vector<int>& v) {
+    v[0] = info.template stride<0>();
+  }
+};
 
 //===------------------------------------------------------------------------------------------===//
 //     Strides
@@ -40,15 +66,23 @@ int get_stride_helper(int coord, const Container& container) noexcept {
 
 template <typename MetaDataType, typename StorageType>
 std::vector<int> get_strides(const StorageType&, const MetaDataType& meta_data) {
-  const auto& strides_array = meta_data.m_strides;
-  const int n_dimensions = meta_data.dims().n_dimensions;
+  //  const auto& strides_array = meta_data.m_strides;
+  //  const int n_dimensions = meta_data.dims().n_dimensions;
+  //
+  //  std::vector<int> strides_vec(n_dimensions);
+  //  for(int i = 0; i < n_dimensions; ++i)
+  //    strides_vec[i] = internal::get_stride_helper<typename
+  //    StorageType::storage_info_type::layout>(
+  //        i, strides_array);
+  //
+  //  return strides_vec;
 
-  std::vector<int> strides_vec(n_dimensions);
-  for(int i = 0; i < n_dimensions; ++i)
-    strides_vec[i] = internal::get_stride_helper<typename StorageType::storage_info_type::layout>(
-        i, strides_array);
+  constexpr int n_dimensions = MetaDataType::layout_t::masked_length;
 
-  return strides_vec;
+  std::vector<int> dims_vec(n_dimensions);
+  static_to_vector<n_dimensions - 1>::stride(meta_data, dims_vec);
+
+  return dims_vec;
 }
 
 //===------------------------------------------------------------------------------------------===//
@@ -57,12 +91,10 @@ std::vector<int> get_strides(const StorageType&, const MetaDataType& meta_data) 
 
 template <typename MetaDataType>
 std::vector<int> get_dims(const MetaDataType& meta_data) noexcept {
-  const auto& unaligned_dims_array = meta_data.m_unaligned_dims;
-  const int n_dimensions = meta_data.dims().n_dimensions;
+  constexpr int n_dimensions = MetaDataType::layout_t::masked_length;
 
   std::vector<int> dims_vec(n_dimensions);
-  for(int i = 0; i < n_dimensions; ++i)
-    dims_vec[i] = unaligned_dims_array[i];
+  static_to_vector<n_dimensions - 1>::dim(meta_data, dims_vec);
 
   return dims_vec;
 }
@@ -87,21 +119,25 @@ int left_padding_helper(unsigned int coord) noexcept {
 template <typename StorageType, class MetaDataType>
 void* get_origin_ptr(const StorageType& storage, const MetaDataType& meta_data,
                      unsigned int field_idx) noexcept {
-  const int n_dimensions = meta_data.dims().n_dimensions;
-  const auto& strides_array = meta_data.m_strides;
+  //  const int n_dimensions = meta_data.dims().n_dimensions;
+  //  const auto& strides_array = meta_data.m_strides;
+  //
+  //  auto* data_ptr = storage.fields()[field_idx].get();
+  //
+  //  for(int i = 0; i < n_dimensions; ++i) {
+  //    int lpad = left_padding_helper<typename StorageType::storage_info_type::layout,
+  //                                   StorageType::storage_info_type::s_alignment,
+  //                                   typename StorageType::storage_info_type::halo_t>(i);
+  //    int stride = internal::get_stride_helper<typename StorageType::storage_info_type::layout>(
+  //        i, strides_array);
+  //    data_ptr += lpad * stride;
+  //  }
 
-  auto* data_ptr = storage.fields()[field_idx].get();
-
-  for(int i = 0; i < n_dimensions; ++i) {
-    int lpad = left_padding_helper<typename StorageType::storage_info_type::layout,
-                                   StorageType::storage_info_type::s_alignment,
-                                   typename StorageType::storage_info_type::halo_t>(i);
-    int stride = internal::get_stride_helper<typename StorageType::storage_info_type::layout>(
-        i, strides_array);
-    data_ptr += lpad * stride;
-  }
-
-  return static_cast<void*>(data_ptr);
+  auto* data_ptr = storage.get_storage_ptr()->get_cpu_ptr();
+  auto index = meta_data.index({
+      0,
+  });
+  return static_cast<void*>(data_ptr + index);
 }
 
 } // namespace internal
--- test/serialbox/core/frontend/gridtools/UnittestStorageView.cpp
+++ test/serialbox/core/frontend/gridtools/UnittestStorageView.cpp
@@ -30,6 +30,10 @@ public:
   using storage_types = serialbox::unittest::gridtools_storage_types<T>;
 
   // Dimensions
+  int dim1_no_halo;
+  int dim2_no_halo;
+  int dim3_no_halo;
+  int dim4_no_halo;
   int dim1;
   int dim2;
   int dim3;
@@ -39,21 +43,21 @@ public:
   typename storage_types::cpu_2d_real_meta_data_type cpu_2d_real_meta_data;
   typename storage_types::gpu_2d_real_meta_data_type gpu_2d_real_meta_data;
   typename storage_types::cpu_2d_meta_data_type cpu_2d_meta_data;
-  typename storage_types::gpu_2d_meta_data_type gpu_2d_meta_data;
-  typename storage_types::cpu_3d_meta_data_type cpu_3d_meta_data;
-  typename storage_types::gpu_3d_meta_data_type gpu_3d_meta_data;
-  typename storage_types::cpu_4d_meta_data_type cpu_4d_meta_data;
-  typename storage_types::gpu_4d_meta_data_type gpu_4d_meta_data;
+  //  typename storage_types::gpu_2d_meta_data_type gpu_2d_meta_data;
+  //  typename storage_types::cpu_3d_meta_data_type cpu_3d_meta_data;
+  //  typename storage_types::gpu_3d_meta_data_type gpu_3d_meta_data;
+  //  typename storage_types::cpu_4d_meta_data_type cpu_4d_meta_data;
+  //  typename storage_types::gpu_4d_meta_data_type gpu_4d_meta_data;
 
   // Storages
   typename storage_types::cpu_2d_real_storage_type cpu_2d_real_storage;
   typename storage_types::gpu_2d_real_storage_type gpu_2d_real_storage;
   typename storage_types::cpu_2d_storage_type cpu_2d_storage;
-  typename storage_types::gpu_2d_storage_type gpu_2d_storage;
-  typename storage_types::cpu_3d_storage_type cpu_3d_storage;
-  typename storage_types::gpu_3d_storage_type gpu_3d_storage;
-  typename storage_types::cpu_4d_storage_type cpu_4d_storage;
-  typename storage_types::gpu_4d_storage_type gpu_4d_storage;
+  //  typename storage_types::gpu_2d_storage_type gpu_2d_storage;
+  //  typename storage_types::cpu_3d_storage_type cpu_3d_storage;
+  //  typename storage_types::gpu_3d_storage_type gpu_3d_storage;
+  //  typename storage_types::cpu_4d_storage_type cpu_4d_storage;
+  //  typename storage_types::gpu_4d_storage_type gpu_4d_storage;
 
 private:
   template <typename Storage>
@@ -101,37 +105,39 @@ private:
 
 protected:
   GridToolsStorageViewTest()
-      : dim1(2 + storage_types::halo1_left + storage_types::halo1_right),
+      : dim1_no_halo(2), dim2_no_halo(3), dim3_no_halo(4), dim4_no_halo(5),
+        dim1(2 + storage_types::halo1_left + storage_types::halo1_right),
         dim2(3 + storage_types::halo2_left + storage_types::halo2_right),
         dim3(4 + storage_types::halo3_left + storage_types::halo3_right),
         dim4(5 + storage_types::halo4_left + storage_types::halo4_right),
-        cpu_2d_real_meta_data(dim1, dim2),                                 //
-        gpu_2d_real_meta_data(dim1, dim2),                                 //
-        cpu_2d_meta_data(dim1, dim2, 0),                                   //
+        cpu_2d_real_meta_data(dim1_no_halo, dim2_no_halo),                                       //
+        gpu_2d_real_meta_data(dim1_no_halo, dim2_no_halo),                                       //
+        cpu_2d_meta_data(dim1_no_halo, dim2_no_halo, 1), /*                                  //
         gpu_2d_meta_data(dim1, dim2, 0),                                   //
         cpu_3d_meta_data(dim1, dim2, dim3),                                //
         gpu_3d_meta_data(dim1, dim2, dim3),                                //
         cpu_4d_meta_data(dim1, dim2, dim3, dim4),                          //
-        gpu_4d_meta_data(dim1, dim2, dim3, dim4),                          //
-        cpu_2d_real_storage(cpu_2d_real_meta_data, "cpu_2d_real_storage"), //
-        gpu_2d_real_storage(gpu_2d_real_meta_data, "gpu_2d_real_storage"), //
-        cpu_2d_storage(cpu_2d_meta_data, "cpu_2d_storage"),                //
+        gpu_4d_meta_data(dim1, dim2, dim3, dim4),                         */ //
+        cpu_2d_real_storage(cpu_2d_real_meta_data, "cpu_2d_real_storage"),                       //
+        gpu_2d_real_storage(gpu_2d_real_meta_data, "gpu_2d_real_storage"),                       //
+        cpu_2d_storage(cpu_2d_meta_data, "cpu_2d_storage") /*,                //
         gpu_2d_storage(gpu_2d_meta_data, "gpu_2d_storage"),                //
         cpu_3d_storage(cpu_3d_meta_data, "cpu_3d_storage"),                //
         gpu_3d_storage(gpu_3d_meta_data, "gpu_3d_storage"),                //
         cpu_4d_storage(cpu_4d_meta_data, "cpu_4d_storage"),                //
-        gpu_4d_storage(gpu_4d_meta_data, "gpu_4d_storage") {
+        gpu_4d_storage(gpu_4d_meta_data, "gpu_4d_storage") */ {
 
     init2DReal(cpu_2d_real_storage);
     init2DReal(gpu_2d_real_storage);
+
     init2D(cpu_2d_storage);
-    init2D(gpu_2d_storage);
-
-    init3D(cpu_3d_storage);
-    init3D(gpu_3d_storage);
-
-    init4D(cpu_4d_storage);
-    init4D(gpu_4d_storage);
+    //    init2D(gpu_2d_storage);
+    //
+    //    init3D(cpu_3d_storage);
+    //    init3D(gpu_3d_storage);
+    //
+    //    init4D(cpu_4d_storage);
+    //    init4D(gpu_4d_storage);
   }
 };
 
@@ -155,9 +161,10 @@ serialbox::StorageView make_storage_view(const Storage& storage) {
 
 using namespace serialbox::gridtools;
 #define GET_DIMS_STRIDES_ORIGIN_PTR(storage, prefix)                                               \
-  std::vector<int> prefix##_dims(internal::get_dims(storage.meta_data())); //\
-//std::vector<int> prefix##_strides(internal::get_strides(storage, storage.meta_data()));          \
-  //void* prefix##_origin_ptr = internal::get_origin_ptr(storage, storage.meta_data(), 0);
+  std::vector<int> prefix##_dims(internal::get_dims(*storage.get_storage_info_ptr()));             \
+  std::vector<int> prefix##_strides(                                                               \
+      internal::get_strides(storage, *storage.get_storage_info_ptr()));                            \
+  void* prefix##_origin_ptr = internal::get_origin_ptr(storage, *storage.get_storage_info_ptr(), 0);
 
 TYPED_TEST(GridToolsStorageViewTest, Construction_2DRealCPU) {
   auto& cpu_2d_real_storage = this->cpu_2d_real_storage;
@@ -170,38 +177,35 @@ TYPED_TEST(GridToolsStorageViewTest, Construction_2DRealCPU) {
   EXPECT_EQ(cpu_2d_real_dims[1], this->dim2);
 
   // Strides
-  //  EXPECT_EQ(cpu_2d_real_strides[0], cpu_2d_real_meta_data.template strides<0>());
-  //  EXPECT_EQ(cpu_2d_real_strides[1], cpu_2d_real_meta_data.template strides<1>());
+  EXPECT_EQ(cpu_2d_real_strides[0], cpu_2d_real_meta_data.template stride<0>());
+  EXPECT_EQ(cpu_2d_real_strides[1], cpu_2d_real_meta_data.template stride<1>());
 
   // Data
-  //  EXPECT_EQ(cpu_2d_real_origin_ptr, static_cast<void*>(&cpu_2d_real_storage(0, 0)));
+  auto view = make_host_view(cpu_2d_real_storage);
+  EXPECT_EQ(cpu_2d_real_origin_ptr, static_cast<void*>(&view(0, 0)));
 }
 
-// TYPED_TEST(GridToolsStorageViewTest, Construction_2DCPU) {
-//  //
-//  -----------------------------------------------------------------------------------------------
-//  // 2D CPU Storage
-//  //
-//  -----------------------------------------------------------------------------------------------
-//  auto& cpu_2d_storage = this->cpu_2d_storage;
-//  auto& cpu_2d_meta_data = this->cpu_2d_meta_data;
-//
-//  GET_DIMS_STRIDES_ORIGIN_PTR(cpu_2d_storage, cpu_2d)
-//
-//  // Dimensions
-//  EXPECT_EQ(cpu_2d_dims[0], this->dim1);
-//  EXPECT_EQ(cpu_2d_dims[1], this->dim2);
-//  EXPECT_EQ(cpu_2d_dims[2], 0);
-//
-//  // Strides
-//  EXPECT_EQ(cpu_2d_strides[0], cpu_2d_meta_data.template strides<0>());
-//  EXPECT_EQ(cpu_2d_strides[1], cpu_2d_meta_data.template strides<1>());
-//  EXPECT_EQ(cpu_2d_strides[2], cpu_2d_meta_data.template strides<2>());
-//
-//  // Data
-//  EXPECT_EQ(cpu_2d_origin_ptr, static_cast<void*>(&cpu_2d_storage(0, 0, 0)));
-//}
-//
+TYPED_TEST(GridToolsStorageViewTest, Construction_2DCPU) {
+  auto& cpu_2d_storage = this->cpu_2d_storage;
+  auto& cpu_2d_meta_data = this->cpu_2d_meta_data;
+
+  GET_DIMS_STRIDES_ORIGIN_PTR(cpu_2d_storage, cpu_2d)
+
+  // Dimensions
+  EXPECT_EQ(cpu_2d_dims[0], this->dim1);
+  EXPECT_EQ(cpu_2d_dims[1], this->dim2);
+  EXPECT_EQ(cpu_2d_dims[2], 1);
+
+  // Strides
+  EXPECT_EQ(cpu_2d_strides[0], cpu_2d_meta_data.template stride<0>());
+  EXPECT_EQ(cpu_2d_strides[1], cpu_2d_meta_data.template stride<1>());
+  EXPECT_EQ(cpu_2d_strides[2], cpu_2d_meta_data.template stride<2>());
+
+  // Data
+  auto view = make_host_view(cpu_2d_storage);
+  EXPECT_EQ(cpu_2d_origin_ptr, static_cast<void*>(&view(0, 0, 0)));
+}
+
 // TYPED_TEST(GridToolsStorageViewTest, Construction_3DCPU) {
 //  auto& cpu_3d_storage = this->cpu_3d_storage;
 //  auto& cpu_3d_meta_data = this->cpu_3d_meta_data;
@@ -244,24 +248,26 @@ TYPED_TEST(GridToolsStorageViewTest, Construction_2DRealCPU) {
 //  EXPECT_EQ(cpu_4d_origin_ptr, static_cast<void*>(&cpu_4d_storage(0, 0, 0, 0)));
 //}
 //
-// TYPED_TEST(GridToolsStorageViewTest, Construction_2DRealGPU) {
-//  auto& gpu_2d_real_storage = this->gpu_2d_real_storage;
-//  auto& gpu_2d_real_meta_data = this->gpu_2d_real_meta_data;
-//
-//  GET_DIMS_STRIDES_ORIGIN_PTR(gpu_2d_real_storage, gpu_2d_real)
-//
-//  // Dimensions
-//  EXPECT_EQ(gpu_2d_real_dims[0], this->dim1);
-//  EXPECT_EQ(gpu_2d_real_dims[1], this->dim2);
-//
-//  // Strides
-//  EXPECT_EQ(gpu_2d_real_strides[0], gpu_2d_real_meta_data.template strides<0>());
-//  EXPECT_EQ(gpu_2d_real_strides[1], gpu_2d_real_meta_data.template strides<1>());
-//
-//  // Data
-//  EXPECT_EQ(gpu_2d_real_origin_ptr, static_cast<void*>(&gpu_2d_real_storage(0, 0)));
-//}
-//
+TYPED_TEST(GridToolsStorageViewTest, Construction_2DRealGPU) {
+  auto& gpu_2d_real_storage = this->gpu_2d_real_storage;
+  auto& gpu_2d_real_meta_data = this->gpu_2d_real_meta_data;
+
+  GET_DIMS_STRIDES_ORIGIN_PTR(gpu_2d_real_storage, gpu_2d_real)
+
+  // Dimensions
+  EXPECT_EQ(gpu_2d_real_dims[0], this->dim1);
+  EXPECT_EQ(gpu_2d_real_dims[1], this->dim2);
+
+  // Strides
+  EXPECT_EQ(gpu_2d_real_strides[0], gpu_2d_real_meta_data.template stride<0>());
+  EXPECT_EQ(gpu_2d_real_strides[1], gpu_2d_real_meta_data.template stride<1>());
+
+  // Data
+
+  auto view = make_host_view(gpu_2d_real_storage);
+  EXPECT_EQ(gpu_2d_real_origin_ptr, static_cast<void*>(&view(0, 0)));
+}
+
 // TYPED_TEST(GridToolsStorageViewTest, Construction_2DGPU) {
 //  auto& gpu_2d_storage = this->gpu_2d_storage;
 //  auto& gpu_2d_meta_data = this->gpu_2d_meta_data;
@@ -431,7 +437,8 @@ TYPED_TEST(GridToolsStorageViewTest, Construction_2DRealCPU) {
 //  // Create a memcopyable storage
 //  using layout_type = gridtools::layout_map<2, 1, 0>; // stride 1 on i (col-major)
 //  using meta_data_type = typename GridToolsStorageViewTest<
-//      TypeParam>::storage_types::storage_traits_type::template meta_storage_type<9, layout_type>;
+//      TypeParam>::storage_types::storage_traits_type::template meta_storage_type<9,
+//      layout_type>;
 //  using storage_type = typename GridToolsStorageViewTest<TypeParam>::storage_types::
 //      storage_traits_type::template storage_type<TypeParam, meta_data_type>;
 //
