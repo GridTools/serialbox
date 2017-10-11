50fb9b9ff947eba667bc049e73d6fa8367a2a0b1
Fabian <fabian_thuering@hotmail.com>
2017-10-11 13:45:33 +0200

    add osx specific build process

--- tools/travis/driver-test.sh
+++ tools/travis/driver-test.sh
@@ -10,29 +10,78 @@
 
 this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Setup dependencies
+# Load cache
 source "$this_script_dir/install.sh"
-install_driver -i ${CACHE_DIR} -b ${PACKAGES_TO_INSTALL}
 
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+  if ! brew ls --version cmake &>/dev/null; then brew install cmake; fi
+  if ! brew ls --version boost &>/dev/null; then brew install boost; fi
+
+  brew update
+  brew install python3
+  pip3 install numpy nose
+      
+  brew tap homebrew/science
+  brew install netcdf
+  export PATH="/usr/local/bin:${PATH}"
+
+else # Linux 
+  install_driver -i ${CACHE_DIR} -b cmake,boost
+
+  if [ ! -z ${CLANG_VERSION+x} ]; then
+    install_driver -i ${CACHE_DIR} -b clang
+  fi
+
+  export SERIALBOX_PYTHON_DIR=/opt/python/3.5.3
+  "$SERIALBOX_PYTHON_DIR/bin/pip3" install nose numpy
+fi
+  
 export CXX=${CXX_COMPILER}
 export CC=${C_COMPILER}
 
 $CC --version
 $CXX --version
 
-# Build dawn
-pushd "$(pwd)"
+# Build Serialbox2
+if [[ "${FC_COMPILER}" != "" ]]; then
+  export SERIALBOX_ENABLE_FORTRAN=ON
+else
+  export SERIALBOX_ENABLE_FORTRAN=OFF
+fi
 
-mkdir build && cd build
-cmake .. -DCMAKE_CXX_COMPILER="$CXX"                                                               \
-         -DCMAKE_C_COMPILER="$CC"                                                                  \
-         -DCMAKE_BUILD_TYPE="$CONFIG"                                                              \
-         -DProtobuf_DIR="$Protobuf_DIR"                                                            \
-      || fatal_error "failed to configure"
-make -j2 || fatal_error "failed to build"
+pushd $(pwd)
+mkdir -p build && cd build
 
-# Run unittests
-ctest -C ${CONFIG} --output-on-failure --force-new-ctest-process                                   \
-     || fatal_error "failed to run tests"
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  cmake ..                                                                                         \
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                                                     \
+        -DSERIALBOX_TESTING=ON                                                                     \
+        -DSERIALBOX_ENABLE_FORTRAN=OFF
+
+else # Linux 
+  cmake ..                                                                                         \
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                                                     \
+        -DPYTHON_EXECUTABLE="$SERIALBOX_PYTHON_DIR/bin/python3"                                    \
+        -DSERIALBOX_TESTING=ON                                                                     \
+        -DSERIALBOX_ENABLE_FORTRAN=$SERIALBOX_ENABLE_FORTRAN                                       \
+        -DBOOST_ROOT="$BOOST_ROOT"
+fi
 
 popd
+
+# Run Python, C and C++ unittests
+pushd $(pwd)
+cd build
+
+ctest -C ${CONFIG} --output-on-failure --force-new-ctest-process                                   \
+     || fatal_error "failed to run unittests"
+
+popd
+
+# Run stand-alone Fortran example
+if [[ "${FC_COMPILER}" != "" ]]; then
+  pushd $(pwd)
+  cd examples/fortran/perturbation/build || fatal_error "failed to run stand-alone Fortran example"
+  bash run.sh
+  popd
+fi
