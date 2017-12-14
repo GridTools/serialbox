6c0e1de92c3a8fd3ef864fd39cf48743924a13eb
Hannes Vogt <hannes@havogt.de>
2017-12-14 20:53:36 +0100

    fix includes

--- test/serialbox/core/frontend/gridtools/UnittestSerializer.cpp
+++ test/serialbox/core/frontend/gridtools/UnittestSerializer.cpp
@@ -13,13 +13,13 @@
 //===------------------------------------------------------------------------------------------===//
 
 #include "utility/GridTools.h"
+#ifdef SERIALBOX_HAS_GRIDTOOLS
 #include "utility/SerializerTestBase.h"
 #include "utility/Storage.h"
-#include "serialbox/core/frontend/gridtools/Serializer.h"
 #include <gtest/gtest.h>
 #include <memory>
+#include "serialbox/core/frontend/gridtools/Serializer.h"
 
-#ifdef SERIALBOX_HAS_GRIDTOOLS
 using namespace serialbox::gridtools;
 using namespace serialbox::unittest;
 
