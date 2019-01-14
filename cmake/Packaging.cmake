0417cb6dc13a5bc3bd4a49d6d2b8edc03a834272
Hannes Vogt <hannes@havogt.de>
2019-01-14 17:25:49 +0100

    fix netcdf export and cleanup examples

--- cmake/Packaging.cmake
+++ cmake/Packaging.cmake
@@ -13,6 +13,7 @@ export(PACKAGE Serialbox)
 include(CMakePackageConfigHelpers)
 
 set(CMAKE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/cmake)
+set(PYTHON_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/python)
 
 install(EXPORT SerialboxTargets
     FILE SerialboxTargets.cmake
@@ -24,7 +25,7 @@ install(EXPORT SerialboxTargets
 configure_package_config_file(${CMAKE_SOURCE_DIR}/cmake/SerialboxConfig.cmake.in
     "${PROJECT_BINARY_DIR}/cmake/SerialboxConfig.cmake"
     INSTALL_DESTINATION ${CMAKE_INSTALL_DIR}
-    PATH_VARS CMAKE_INSTALL_DIR
+    PATH_VARS CMAKE_INSTALL_DIR PYTHON_INSTALL_DIR
 )
 install(FILES "${PROJECT_BINARY_DIR}/cmake/SerialboxConfig.cmake" DESTINATION cmake)
 
@@ -44,11 +45,31 @@ export(TARGETS SerialboxStatic SerialboxObjects SerialboxFilesytemTarget
     FILE ${PROJECT_BINARY_DIR}/SerialboxTargets.cmake
     NAMESPACE Serialbox::
 )
+if(SERIALBOX_HAS_NETCDF)
+    export(TARGETS NETCDF_TARGET
+        APPEND FILE ${PROJECT_BINARY_DIR}/SerialboxTargets.cmake
+        NAMESPACE Serialbox::
+    )
+endif()
+if(SERIALBOX_ENABLE_C)
+    export(TARGETS SerialboxCStatic SerialboxCObjects
+        APPEND FILE ${PROJECT_BINARY_DIR}/SerialboxTargets.cmake
+        NAMESPACE Serialbox::
+    )
+endif()
+if(SERIALBOX_ENABLE_FORTRAN)
+    export(TARGETS SerialboxFortranStatic SerialboxFortranObjects SerialboxFortranSerializeObjects
+        APPEND FILE ${PROJECT_BINARY_DIR}/SerialboxTargets.cmake
+        NAMESPACE Serialbox::
+    )
+endif()
 
+set(CMAKE_INSTALL_DIR ${PROJECT_SOURCE_DIR}/cmake)
+set(PYTHON_INSTALL_DIR ${PROJECT_SOURCE_DIR}/src/serialbox-python)
 configure_package_config_file(${CMAKE_SOURCE_DIR}/cmake/SerialboxConfig.cmake.in
     ${PROJECT_BINARY_DIR}/SerialboxConfig.cmake
     INSTALL_DESTINATION ${PROJECT_BINARY_DIR}
-    PATH_VARS CMAKE_INSTALL_DIR
+    PATH_VARS CMAKE_INSTALL_DIR PYTHON_INSTALL_DIR
 )
 write_basic_package_version_file(
   ${PROJECT_BINARY_DIR}/SerialboxConfigVersion.cmake
