84e48fca8a5693cf25ebf9141ae3ce9968d83fa2
Hannes Vogt <hannes@havogt.de>
2019-02-01 16:50:29 +0100

    cleanup

--- cmake/modules/FindClangTools.cmake
+++ cmake/modules/FindClangTools.cmake
@@ -18,28 +18,22 @@
 
 find_program(CLANG_TIDY_BIN 
   NAMES clang-tidy-6.0.0 clang-tidy
-  PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin 
-        NO_DEFAULT_PATH
+  PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} $ENV{PATH} 
 )
 
-if ("${CLANG_TIDY_BIN}" STREQUAL "CLANG_TIDY_BIN-NOTFOUND") 
-  set(CLANG_TIDY_FOUND 0)
+if(NOT CLANG_TIDY_BIN) 
   message(STATUS "clang-tidy not found")
 else()
-  set(CLANG_TIDY_FOUND 1)
   message(STATUS "clang-tidy found at ${CLANG_TIDY_BIN}")
 endif()
 
 find_program(CLANG_FORMAT_BIN 
   NAMES clang-format-6.0.0 clang-format
-  PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin 
-        NO_DEFAULT_PATH
+  PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} $ENV{PATH} 
 )
 
-if("${CLANG_FORMAT_BIN}" STREQUAL "CLANG_FORMAT_BIN-NOTFOUND") 
-  set(CLANG_FORMAT_FOUND 0)
+if(NOT CLANG_FORMAT_BIN) 
   message(STATUS "clang-format not found")
 else()
-  set(CLANG_FORMAT_FOUND 1)
   message(STATUS "clang-format found at ${CLANG_FORMAT_BIN}")
 endif()
