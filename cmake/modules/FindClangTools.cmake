41aa9200a36116abbfcef73cd3bf97ee91ba55d5
Hannes Vogt <hannes@havogt.de>
2019-02-01 16:29:50 +0100

    reenable clang tools

--- cmake/modules/FindClangTools.cmake
+++ cmake/modules/FindClangTools.cmake
@@ -17,7 +17,7 @@
 #
 
 find_program(CLANG_TIDY_BIN 
-  NAMES clang-tidy-3.8 clang-tidy-3.7 clang-tidy-3.6 clang-tidy
+  NAMES clang-tidy-6.0.0 clang-tidy
   PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin 
         NO_DEFAULT_PATH
 )
@@ -31,7 +31,7 @@ else()
 endif()
 
 find_program(CLANG_FORMAT_BIN 
-  NAMES clang-format-3.8 clang-format-3.7 clang-format-3.6  clang-format
+  NAMES clang-format-6.0.0 clang-format
   PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin 
         NO_DEFAULT_PATH
 )
