2c259d9754c7853977f08f22b1164b6b0c63c3cb
Hannes Vogt <hannes@havogt.de>
2019-07-05 15:38:17 +0200

    fix typo

--- cmake/modules/SerialboxTestScript.cmake
+++ cmake/modules/SerialboxTestScript.cmake
@@ -2,7 +2,7 @@
 ##
 ##                                   S E R I A L B O X
 ##
-## This file is distributed under terms of BSD license. 
+## This file is distributed under terms of BSD license.
 ## See LICENSE.txt for more information.
 ##
 ##===------------------------------------------------------------------------------------------===##
@@ -22,23 +22,23 @@ endfunction(serialbox_test_init)
 
 ## serialbox_add_test
 ## ------------------
-## 
+##
 ## Add a unittest to CTest and the test_script.
 ##
-##    TARGET:STRINGS=<>          - The first string in the list will be treated as the CMake target 
+##    TARGET:STRINGS=<>          - The first string in the list will be treated as the CMake target
 ##                                 to run i.e the file $<TARGET_FILE:target> will be added to CTest
-##                                 while the remaining elements in the list are passed as 
+##                                 while the remaining elements in the list are passed as
 ##                                 command-line arguments.
-##    EXECUTABLE:STRINGS=<>      - The first string in the list will be treated as the exectuable 
-##                                 to run while the remaining elements in the list are passed as 
+##    EXECUTABLE:STRINGS=<>      - The first string in the list will be treated as the executable
+##                                 to run while the remaining elements in the list are passed as
 ##                                 command-line arguments.
 ##    NAME:STRING=<>             - [optional]: Name of the test. If the name is not provided either
-##                                 the CMake target or the second argument to EXECUTABLE will be 
+##                                 the CMake target or the second argument to EXECUTABLE will be
 ##                                 used.
 ##
 function(serialbox_add_test)
   cmake_parse_arguments(serialbox_add_test "" "NAME" "TARGET;EXECUTABLE" ${ARGN})
-  
+
   set(target_list ${serialbox_add_test_TARGET})
   set(executable_list ${serialbox_add_test_EXECUTABLE})
   set(name ${serialbox_add_test_NAME})
@@ -47,44 +47,44 @@ function(serialbox_add_test)
 
   if(target_list)
     list(GET target_list 0 target)
-    
+
     set(args ${target_list})
     list(REMOVE_AT args 0)
-    
+
     set(flat_args "")
     foreach(arg ${args})
       set(flat_args "${flat_args} ${arg}")
     endforeach()
-    
+
     if(NOT(name))
       set(name ${target})
     endif()
-    
+
     add_test(NAME ${name} COMMAND $<TARGET_FILE:${target}> ${flat_args})
     set(test_already_added_to_ctest TRUE)
   endif()
-  
+
   if(executable_list)
-    list(GET exectuable_list 0 exectuable)
-    
+    list(GET executable_list 0 exectuable)
+
     set(args ${executable_list})
     list(REMOVE_AT args 0)
-    
+
     set(flat_args "")
     foreach(arg ${args})
       set(flat_args "${flat_args} ${arg}")
     endforeach()
-    
+
     if(NOT(name))
       list(GET executable_list 1 name_with_whitespaces)
       string(STRIP ${name_with_whitespaces} name)
     endif()
-  
+
     file(APPEND ${SERIALBOX_TEST_SCRIPT} "\n${exectuable} ${flat_args}\n")
     file(APPEND ${SERIALBOX_TEST_SCRIPT} "ret=\$?\n")
     file(APPEND ${SERIALBOX_TEST_SCRIPT} "if [ $ret -ne 0 ] ; then\n echo \"Error: problem found in Unittest\"\nfi\n")
     file(APPEND ${SERIALBOX_TEST_SCRIPT} "res=$((res || ret ))\n")
-    
+
     if(NOT(test_already_added_to_ctest))
       add_test(NAME ${name} COMMAND ${executable_list})
     endif()
@@ -99,7 +99,7 @@ endfunction(serialbox_add_test)
 ## Append last statements to the test_script.
 ##
 function(serialbox_test_end)
-  file(APPEND ${SERIALBOX_TEST_SCRIPT} 
+  file(APPEND ${SERIALBOX_TEST_SCRIPT}
        "\nif [ $res -ne 0 ]; then\n"
        "  printf \"\\n  >>>>>>>>>>>>>>>> TESTS FAILED <<<<<<<<<<<<<<<<\\n\\n\"\n"
        "else\n"
@@ -109,4 +109,3 @@ function(serialbox_test_end)
   file(INSTALL ${SERIALBOX_TEST_SCRIPT} DESTINATION ${PROJECT_BINARY_DIR}/install
        FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ)
 endfunction(serialbox_test_end)
-
