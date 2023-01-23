735c929ba80defc8af0edfcaa95ffebea259596b
Magdalena Luz <luzm@ethz.ch>
2023-01-23 17:39:55 +0100

    need to set CMAKE_MACOSX_RPATH in CMakeLists.txt

--- src/serialbox-python/setup.py
+++ src/serialbox-python/setup.py
@@ -53,7 +53,6 @@ class CMakeBuild(build_ext):
             "-DPYTHON_EXECUTABLE={}".format(sys.executable),
             "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
             "-DSERIALBOX_ENABLE_FORTRAN=false",
-            "-DCMAKE_MACOSX_RPATH=true",
             "-DCMAKE_BUILD_RPATH={}".format(origin),
             "-DSERIALBOX_ENABLE_SDB=false",
             "-DSERIALBOX_ASYNC_API=false",
